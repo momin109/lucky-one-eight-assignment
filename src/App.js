718fda7b76cd0f81112793a0bd119335b886b7a7
momin109 <momin550550@gmail.com>
2022-03-27 11:21:15 +0600

    title add

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Shop from './component/Shop/Shop'
 import Deatail from './component/Deatail/Deatail';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Shop></Shop>
       <Deatail></Deatail>
 
---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='device-arena'>
+            <h1>Device Arena</h1>
+        </div>
+    );
+};
+
+export default Header;
